2be76513fb08061b4d06b2f36494c158fbd13632
ohoCoding <youngho7482@gmail.com>
2025-05-21 03:43:04 +0900

    feat: :sparkles: set router using library

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-import { useDispatch, useSelector } from "react-redux";
-import type { RootState } from "@/app/store";
-import { increment, decrement } from "@/features/counterSlice";
-import { Button } from "@/components/ui/button";
+import { BaseLayout } from "@/layout/BaseLayout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "@/pages/Homepage";
 
 function App() {
-  const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch = useDispatch();
-
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-4">
-      <h1 className="text-2xl font-bold">Counter: {count}</h1>
-      <div className="flex gap-2">
-        <Button onClick={() => dispatch(increment())}>+1</Button>
-        <Button onClick={() => dispatch(decrement())} variant="outline">
-          -1
-        </Button>
-      </div>
-    </div>
+    <BrowserRouter>
+      <BaseLayout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* 기타 라우트 */}
+        </Routes>
+      </BaseLayout>
+    </BrowserRouter>
   );
 }
 
