43a4b22383b0ad42e31da7858aece74f6601b73a
ohoCoding <youngho7482@gmail.com>
2025-06-18 07:16:32 +0900

    feat: :sparkles: 라우트 페이지 구성

---
+++ src/pages/AccessibilityIntro.tsx
@@ -0,0 +1,11 @@
+import NotionEmbed from "@/components/common/NotionEmbed";
+
+const AccessibilityIntro = () => {
+  return (
+    <div className="p-4">
+      <NotionEmbed url="https://access.super.site/" />
+    </div>
+  );
+};
+
+export default AccessibilityIntro;
