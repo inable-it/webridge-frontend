876d32f79e0fb3984d38d5d54166cd4b600915f3
ohoCoding <youngho7482@gmail.com>
2025-06-19 23:26:47 +0900

    feat: :sparkles: GoogleOAuthProvider에 Client ID 넣고 감싸기 & 직접 커스텀 버튼 생성 후 소셜 로그인 호출

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -5,8 +5,9 @@ import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
-import { FcGoogle } from "react-icons/fc";
 import { useNavigate } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import GoogleLoginButton from "@/components/common/GoogleLoginButton";
 
 export const SignupPage = () => {
   const navigate = useNavigate();
@@ -154,14 +155,9 @@ export const SignupPage = () => {
         </div>
 
         {/* 구글 로그인 */}
-        <Button
-          type="button"
-          variant="outline"
-          className="flex items-center justify-center w-full gap-2 bg-gray-100 hover:bg-gray-200"
-        >
-          <FcGoogle className="w-5 h-5" />
-          Google로 계속하기
-        </Button>
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+          <GoogleLoginButton />
+        </GoogleOAuthProvider>
 
         {/* 약관 링크 */}
         <div className="flex justify-center gap-4 mt-2 text-xs text-gray-500">
