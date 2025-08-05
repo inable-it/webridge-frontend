646c0d50855fa3a8ae5254b423c3d16693d5e350
ohoCoding <youngho7482@gmail.com>
2025-08-05 19:43:10 +0900

    feat: :fire: 이메일 전송, 비밀번호 재설정 화면 구현

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import LoginPage from "@/pages/Login";
 import { useRoutes } from "react-router-dom";
 import { SimpleLayout } from "@/layout/SimpleLayout";
 import { BaseLayout } from "@/layout/BaseLayout";
+import { RequireAuth } from "@/components/common/RequireAuth";
+import { Toaster } from "@/components/ui/toaster";
 import TeamIntro from "@/pages/TeamIntro";
 import AccessibilityIntro from "@/pages/AccessibilityIntro";
 import DashboardPage from "@/pages/DashboardPage";
-import { MyInfoPage } from "@/pages/MyInfoPage"; // 추가
-import { FeedbackPage } from "@/pages/FeedbackPage"; // 추가
-import { RequireAuth } from "@/components/common/RequireAuth";
-import { Toaster } from "@/components/ui/toaster";
+import MyInfoPage from "@/pages/MyInfoPage";
+import PasswordResetPage from "@/pages/PasswordResetPage";
+import FeedbackPage from "@/pages/FeedbackPage";
+import PasswordResetConfirmPage from "@/pages/PasswordResetConfirmPage";
 
 function App() {
   const routes = useRoutes([
@@ -23,6 +25,8 @@ function App() {
         { path: "signup", element: <SignupPage /> },
         { path: "team", element: <TeamIntro /> },
         { path: "accessibility", element: <AccessibilityIntro /> },
+        { path: "password-reset", element: <PasswordResetPage /> },
+        { path: "reset-password", element: <PasswordResetConfirmPage /> },
       ],
     },
     {
