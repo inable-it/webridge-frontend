a90864c7602ded1ae71490089514a3c81c48b931
ohoCoding <youngho7482@gmail.com>
2025-08-18 06:47:31 +0900

    feat: :sparkles: 소식지 노션 임베디드 추가

---
+++ src/pages/notion/NewsPage.tsx
@@ -0,0 +1,11 @@
+import NotionEmbed from "@/components/common/NotionEmbed";
+
+const NewsPage = () => {
+  return (
+    <div className="p-4">
+      <NotionEmbed url="https://verbal-bobcat.super.site/" />
+    </div>
+  );
+};
+
+export default NewsPage;
