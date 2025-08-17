cb6883d61e5a6d1521b436b595bac0f41e9fce7a
ohoCoding <youngho7482@gmail.com>
2025-08-18 06:46:12 +0900

    refactor: :recycle: 팀소개, 웹접근성 페이지 수정

--- src/pages/notion/AccessibilityIntro.tsx
+++ src/pages/notion/AccessibilityIntro.tsx
@@ -3,7 +3,7 @@ import NotionEmbed from "@/components/common/NotionEmbed";
 const AccessibilityIntro = () => {
   return (
     <div className="p-4">
-      <NotionEmbed url="https://inable.super.site/accessibility" />
+      <NotionEmbed url="https://urgent-donkey.super.site/%ec%9b%b9-%ec%a0%91%ea%b7%bc%ec%84%b1%ec%9d%b4%eb%9e%80" />
     </div>
   );
 };
--- src/pages/notion/TeamIntro.tsx
+++ src/pages/notion/TeamIntro.tsx
@@ -3,7 +3,7 @@ import NotionEmbed from "@/components/common/NotionEmbed";
 const TeamIntro = () => {
   return (
     <div className="p-4">
-      <NotionEmbed url="https://inable.super.site/team" />
+      <NotionEmbed url="https://urgent-donkey.super.site/inable-team" />
     </div>
   );
 };
